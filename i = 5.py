34f2272411e787b50a728089ca8ff16261a1968e
skswi <166737309+skswi@users.noreply.github.com>
2024-04-13 19:50:34 +0300

    Update i = 5.py

--- i = 5.py
+++ i = 5.py
@@ -2,15 +2,14 @@ import random
 def name(size):
     size = 3 # input maybe later
 def make_new_board():
-    board = [['None' for i in range(size)]for i in range(size)]  # [ none , none ,none]
-def bomb_plante(num_bombs):                                      # [ none , none ,X]
-                                                                 # [ none , none ,none]
+    board = [[0 for i in range(size)]for i in range(size)] 
+def bomb_plante(num_bombs):                                      
     bomb_planted = 0
     while bomb_planted < num_bombs: 
     coords = random.randint(0, size**2-1)  
     row = coords // size 
     col = coords % size
-    if board[row][col] == 'None':
+    if board[row][col] == 0:
         board[row][col] = 'X'
     bomb_planted+=1
 def assign_values_to_board():
@@ -18,14 +17,12 @@ def assign_values_to_board():
         for c in range(size):
             if board[r][c] == 'X':
                 get_num_neighboring_bomb(r,c) 
+i=0
 def get_num_neighboring_bomb(row,col):
-        # top left: (row-1, col-1)
-        # top middle: (row-1, col)
-        # top right: (row-1, col+1)
-        # left: (row, col-1)
-        # right: (row, col+1)
-        # bottom left: (row+1, col-1)
-        # bottom middle: (row+1, col)
-        # bottom right: (row+1, col+1)
-              # for r in range((0, row-1), (size-1, row+1)+1):
-            #for c in range((0, col-1), (size-1, col+1)+1):
+            while i<num_bombs:
+                for r in range(row-1,row+2):
+                    for c in range(col-1,col+2):
+                        if(-1<r<3 and -1<c<3): 
+                            if(board[r][c] != 'X'):
+                                board[r][c]+=1
+            i+=1
