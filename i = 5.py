88c7073e21745346d1aa8186a1a6918fb4292a25
skswi <166737309+skswi@users.noreply.github.com>
2024-04-28 09:24:47 +0300

    Update i = 5.py
    digstart added

--- i = 5.py
+++ i = 5.py
@@ -23,7 +23,7 @@ class game:
         else:
             self.bomb_planted = 0
             while self.bomb_planted  <  self.num_bombs: 
-                self.coords = random.randint(0, self.Length * self.Width -1)  
+                self.coords = random.randint(0, self.Length * self.Width -1)           # 3 by 3 first squeres are open
                 self.row = self.coords // self.Length 
                 self.col = self.coords % self.Width
                 if self.board[self.row][self.col] != 'x':
@@ -37,7 +37,7 @@ class game:
         for r in range(self.row-1,self.row+2):
             for c in range(self.col-1,self.col+2):
                 if(-1<r<self.Width and -1<c<self.Length):
-                    if(self.board[r][c] != 'x'):
+                    if(self.board[r][c] == 0):
                         self.board[r][c]+=1
         if self.bomb_planted == self.num_bombs:
             print(*self.board,sep='\n')
@@ -47,8 +47,10 @@ class game:
     def input_coords(self):
         user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row,col: ")) # 0, 3  
         self.row2, self.col2 = int(user_input[0])-1, int(user_input[-1])-1
-        print(self.row2 , self.col2)   # for us 
-        self.dig(self.row2,self.col2)
+        if self.dig == 0:
+            self.dig(self.row2,self.col2)
+        else:
+            self.digstart(self.row2,self.col2)
 
     def make_visboard(self):
         self.visboard = [['?' for i in range(self.Length)]for i in range(self.Width)]
@@ -85,7 +87,43 @@ class game:
             print("wrong input , not in board")
             self.input_coords()
 
-game()                                       # make visboard after the open of the first block  !!!!!!!!!!!!!!!!
-                                             # randomom bombs but not random ( להגביל עם if ) !!!!!!!!!!!!!!!!!!!
 
+
+    def digstart(self,row2,col2):
+        if(-1<self.row2<self.Width and -1<self.col2<self.Length):
+            if self.board[row2][col2] == 'x':
+                print("gameover")
+                print(*self.board,sep='\n')
+            else:
+              if self.visboard[row2][col2] == self.board[row2][col2]:
+                    print("nah uh , you cant say the same coords twice") 
+                    self.input_coords()
+              else:
+                self.visboard[self.row2][self.col2] = self.board[self.row2][self.col2]
+                self.diged = self.diged + 1
+                for r2 in range(self.row2-1,self.row2+2):
+                    for c2 in range(self.col2-1,self.col2+2):
+                        if(-1<r2<self.Width and -1<c2<self.Length):
+                           if self.board[r2][c2] != 'x' :
+                             if self.visboard[r2][c2] != self.board[r2][c2]:
+                                 self.diged = self.diged + 1
+                                 if self.board[r2][c2]==0:
+                                  for r2 in range(r2-1,r2+2):
+                                   for c2 in range(c2-1,c2+2):
+                                     if(-1<r2<self.Width and -1<c2<self.Length):
+                                        if self.board[r2][c2] != 'x' :
+                                          if self.visboard[r2][c2] != self.board[r2][c2]:
+                                           self.visboard[r2][c2] = self.board[r2][c2]
+                                           self.diged = self.diged + 1
+        else:
+          print("wrong input , not in board")
+          self.input_coords()
+    
+        print(*self.visboard,sep='\n')
+        self.input_coords()
+
+
+
+
+game()
 
