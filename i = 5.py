a81ef79b6f9d92a3bbc3cb2cb738d8ab9add36d4
skswi <166737309+skswi@users.noreply.github.com>
2024-05-27 14:07:54 +0300

    Update i = 5.py

--- i = 5.py
+++ i = 5.py
@@ -41,7 +41,7 @@ pygame.init()
 pygame.display.set_caption("Minesweeper ") 
 window = pygame.display.set_mode((length_window,width_window))
 pygame.display.flip()
-white = [255, 255, 255]
+white = (255, 255, 255)
 black = (0,0,0)
 green = (0, 255, 0)
 color = (140,140,140)
